879e8862b0cfb590a1a0c25ba726a05dd5187eab
Jonathan Siegel <siegeljm@gmail.com>
2012-11-24 22:30:16 -0500

    Make a basic User model (including secure passwords) updated after exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,13 +102,23 @@ describe User do
 
   	describe "with valid password" do
   		it { should == found_user.authenticate(@user.password) }
-	end
-	describe "with invalid password" do
+      end
+	 describe "with invalid password" do
 		let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
 		it { should_not == user_for_invalid_password }
 		specify { user_for_invalid_password.should be_false }
-	end
+  	end
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
 
 end
